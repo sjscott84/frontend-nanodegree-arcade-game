00904f40494632aa65050586cccfbd55a1682b83
Sarah Scott <s.j.scott84@gmail.com>
2015-12-03 12:22:59 -0800

    Level updates only when all gems are collected AND player reaches water

--- js/app.js
+++ js/app.js
@@ -21,6 +21,10 @@ var screenWidth = 505,
     heartWidth = 34,
     heartTopSpace = 1.
     heartSideSpace = 1,
+    levelOneGems = 5,
+    levelTwoGems = 10,
+    levelThreeGems = 15,
+    safe = 81,
     score = 0;
 
 //Create the enemy object
@@ -89,6 +93,7 @@ Player.prototype.update = function(){
 }
 
 //Reset the player to starting position after a collision
+//TODO: this currently loses a life if gems are collected while on the ground
 Player.prototype.reset = function(){
     if(this.y + playerTopSpace > playerVerticalMove){//this resets the player without losing a life when player gets to the water
         this.lives --;
@@ -116,7 +121,15 @@ Player.prototype.collideEnemy = function(){
 Player.prototype.collideGem = function(){
     if(isCollisionWith(player, gem)){
         score.update();
-        gem.update();
+        if(level.level === 1 && score.gems < levelOneGems){
+            gem.update();
+        }else if(level.level === 2 && score.gems < levelTwoGems){
+            gem.update();
+        }else if(level.level === 3 && score.gems < levelThreeGems){
+            gem.update();
+        }else{
+            delete gem.gem;
+        }
     }
 }
 
@@ -132,44 +145,37 @@ Player.prototype.collideHeart = function(){
 //Moves player around the screen
 Player.prototype.handleInput = function(key){
     switch (key){
-        case "left":
+        case 'left':
             if(this.x>tileWidth-1){
                 this.x = this.x - playerSideMove;
             };
             this.collideGem();
             this.collideHeart();
             break;
-        case "up":
+        case 'up':
             if(this.y + playerTopSpace > playerVerticalMove){
                 this.y = this.y - playerVerticalMove;
                     if(this.y + playerTopSpace < playerVerticalMove){
-                        setTimeout(function() {player.reset()}.bind(player.reset), 1000);
+                        score.check();
                     };
             };
             this.collideGem();
             this.collideHeart();
             break;
-        case "right":
+        case 'right':
             if(this.x<screenWidth - tileWidth){
                 this.x = this.x + playerSideMove;
             };
             this.collideGem();
             this.collideHeart();
             break;
-        case "down":
-            //if(level.level === 1){
+        case 'down':
                 if(this.y + playerTopSpace < screenHeight - tileHeight*2){
                     this.y = this.y + playerVerticalMove;
                 }
-            //}else{
-                //if (this.y + playerTopSpace < screenHeightNew - tileHeight*2){
-                    //this.y = this.y + playerVerticalMove;
-                //}
-            //}
             this.collideGem();
             this.collideHeart();
     };
-
 }
 
 //Create Gem object
@@ -181,6 +187,7 @@ var Gem = function(x, y, h, w, top, side, image){
     this.h = h;
     this.top = top;
     this.side = side;
+    this.match = true;
 }
 
 //Draw gem on screen
@@ -192,15 +199,14 @@ Gem.prototype.render = function() {
 
 //Updates gem to new location when there is a collision ensuring the gem does not appear in the same spot twice in a row
 Gem.prototype.update = function() {
-        var oldGem = this;
-        var match = true;
-        gem = new Gem(gemX(), gemY(), gemHeight, gemWidth, gemTopSpace, gemSideSpace, 'images/Gem Blue Small.png');
-        while(match === true){
+    var oldGem = this;
+    gem = new Gem(gemX(), gemY(), gemHeight, gemWidth, gemTopSpace, gemSideSpace, 'images/Gem Blue Small.png');
+        while(this.match === true){
             if(gem.x === oldGem.x && gem.y === oldGem.y){
                 gem = new Gem(gemX(), gemY(), gemHeight, gemWidth, gemTopSpace, gemSideSpace, 'images/Gem Blue Small.png');
             }else{
                 gem.render();
-                match = false;
+                this.match = false;
             }
         }
 }
@@ -215,8 +221,8 @@ var Heart = function(x, y, image){
 
 //Draw the life count on screen
 Heart.prototype.render = function(){
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "white";
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'white';
     if(Resources.get(this.heart)){
         ctx.drawImage(Resources.get(this.heart), this.x + 45, this.y - 27);
     };
@@ -227,9 +233,9 @@ Heart.prototype.render = function(){
 //Update the life count on collision with enemy
 Heart.prototype.update = function (){
     if(player.lives < 0){
-        Alert.render("Sorry, you lose");
+        Alert.render('Sorry, you lose');
     }else{
-       this.count = player.lives + " x ";
+       this.count = player.lives + ' x ';
     }
 }
 
@@ -268,29 +274,31 @@ SafeHeart.prototype.newLife = function(){
 var Score = function(x, y){
     this.x = x;
     this.y = y;
-    this.points = 0;
-    this.display = "Score: " + this.points;
+    this.gems = 0;
+    this.display = 'Gems: ' + this.gems;
 }
 
 //Draw gems on screen
 Score.prototype.render = function(){
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "white";
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'white';
     ctx.fillText(this.display, this.x, this.y);
     ctx.strokeText(this.display, this.x, this.y);
 }
 
 //Updates the score on collision with gems
 Score.prototype.update = function(){
-    this.points ++;
-    this.display = "Score: " + this.points;
+    this.gems ++;
+    this.display = 'Gems: ' + this.gems;
+}
 
+Score.prototype.check = function(){
     if(level.level === 1){
-        level.score(5);
+        level.score(levelOneGems);
     }else if(level.level === 2){
-        level.score(10);
+        level.score(levelTwoGems);
     }else{
-        level.score(15);
+        level.score(levelThreeGems);
     }
 }
 
@@ -299,13 +307,13 @@ var Level = function(x, y){
     this.x = x;
     this.y = y;
     this.level = 1;
-    this.display = "Level " + this.level;
+    this.display = 'Level ' + this.level;
 }
 
 //Draw the level number on screen
 Level.prototype.render = function(){
-    ctx.font = "30px Arial";
-    ctx.fillStyle = "white";
+    ctx.font = '30px Arial';
+    ctx.fillStyle = 'white';
     ctx.fillText(this.display, this.x, this.y);
     ctx.strokeText(this.display, this.x, this.y);
 }
@@ -313,27 +321,31 @@ Level.prototype.render = function(){
 //Update the level when enough gems are collected
 Level.prototype.update = function(){
     this.level++;
-    this.display = "Level " + this.level;
+    this.display = 'Level ' + this.level;
     safeHeart.oneChance = true;
     level.render();
 }
 
 //Indicates if player has won the level or the game based on score/number of gems collected
 Level.prototype.score = function(points){
-    if(score.points === points){
-        if(points === 6){
-            Alert.render("Yay, you won the game!")
+        if(score.gems === points && player.y + playerTopSpace < safe){
+            if(points === levelThreeGems){
+                Alert.render('Yay, you won the game!');
+            }else{
+                setTimeout(function() {
+                    Alert.render('Yay, you won this level!');
+                    level.update();
+                    score.gems = 0;
+                    score.display = 'Gems: ' + score.gems;
+                    player.reset();
+                    level.enemies();
+                    gem = gem = new Gem (gemX(), gemY(), gemHeight, gemWidth, gemTopSpace, gemSideSpace, 'images/Gem Blue Small.png');
+                    gem.render();
+                }, 500);
+            }
         }else{
-            setTimeout(function() {
-                Alert.render("Yay, you won this level!");
-                level.update();
-                score.points = 0;
-                score.display = "Score: " + score.points;
-                player.reset();
-                level.enemies();
-            }, 500);
+            setTimeout(function() {player.reset()}.bind(player.reset), 1000);
         }
-    }
 }
 
 //Adds an extra enemy to screen on level 2
@@ -355,41 +367,41 @@ var CustomAlert = function (){
 
 //Game instructions and start game button
 CustomAlert.prototype.instructions = function (){
-    this.dialogoverlay.style.height = this.winH+"px";
-    this.startBox.style.top = 200+"px";
-    this.startBox.style.left = (this.winW/2)-(450 * .5)+"px";
+    this.dialogoverlay.style.height = this.winH + 'px';
+    this.startBox.style.top = 100 + 'px';
+    this.startBox.style.left = (this.winW / 2) - (450 * .5) + 'px';
     document.getElementById('instructions').innerHTML = instructions;
     document.getElementById('startGame').innerHTML = '<button onclick="start.start()">Start Game!</button>';
 }
 
 //Start Game button
 CustomAlert.prototype.start = function(){
-    this.dialogoverlay.style.display = "none";
-    this.startBox.style.display = "none";
+    this.dialogoverlay.style.display = 'none';
+    this.startBox.style.display = 'none';
 }
 
 //Level, game win and lose alerts
 CustomAlert.prototype.render = function(dialog){
-    this.dialogoverlay.style.display = "block";
-    this.dialogoverlay.style.height = this.winH+"px";
-    this.dialogbox.style.display = "block";
-    this.dialogbox.style.top = 200+"px";
-    this.dialogbox.style.left = (this.winW/2)-(450 * .5)+"px";
+    this.dialogoverlay.style.display = 'block';
+    this.dialogoverlay.style.height = this.winH + 'px';
+    this.dialogbox.style.display = 'block';
+    this.dialogbox.style.top = 200 + 'px';
+    this.dialogbox.style.left = (this.winW / 2) - (450 * .5) + 'px';
     document.getElementById('dialogboxbody').innerHTML = dialog;
     document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
 }
 
 //OK button on alerts
 CustomAlert.prototype.ok = function(){
-    this.dialogbox.style.display = "none";
-    this.dialogoverlay.style.display = "none";
-    if(player.lives < 0 || score.points === 6){
+    this.dialogbox.style.display = 'none';
+    this.dialogoverlay.style.display = 'none';
+    if(player.lives < 0 || score.gems === levelThreeGems){
         reload();
     }
 }
 
 //Instatiate objects
-var instructions = "aslkjdh;ksjhg;asfg;sldhg<br>askjdfhsakjghksajhga<br>laksdflksdjhgk;sajhgklsgjh";
+var instructions = "Level 1 - collect 5 gems<br>Level 2 - collect 10 gems<br>Level 3 - collect 15 gems<br>Complete all 3 levels to win game!<br><br>Water and grass are safe<br>but entering the water will reset your player.<br><br>Have fun and good luck!<br><br>Pssst...keep an eye out for the extra lives.";
 var allEnemies = [];
 var level = new Level(205, 100);
 var gem = new Gem (gemX(), gemY(), gemHeight, gemWidth, gemTopSpace, gemSideSpace, 'images/Gem Blue Small.png');
@@ -404,7 +416,7 @@ function initialEnemies(){
     allEnemies.push(new Enemy(1, 60, enemeyHeight, enemyWidth, enemyTopSpace, enemySideSpace, 'images/enemy-bug.png'));
     allEnemies.push(new Enemy(202, 140, enemeyHeight, enemyWidth, enemyTopSpace, enemySideSpace, 'images/enemy-bug.png'));
 
-    for(var i=0; i<allEnemies.length; i++){
+    for(var i = 0; i < allEnemies.length; i++){
         allEnemies[i].render();
     };
 }
@@ -429,8 +441,6 @@ function isCollisionWith(player, thing) {
         playerAdjustedX + player.w > thingAdjustedX &&
         playerAdjustedY < thingAdjustedY + thing.h &&
         playerAdjustedY + player.h > thingAdjustedY) {
-        
-        console.log("COLLISION");
         return true;
     }
 }
@@ -438,16 +448,16 @@ function isCollisionWith(player, thing) {
 //functions to generate random numbers
 function randomIntFromInterval(){ 
     if(level.level === 1){ 
-        return Math.floor(Math.random()*(300-100+1)+100);
+        return Math.floor(Math.random() * (300 - 100 + 1) + 100);
     }else if(level.level === 2){
-        return Math.floor(Math.random()*(400-200+1)+200);
+        return Math.floor(Math.random()*(400 - 200 + 1) + 200);
     }else{
-        return Math.floor(Math.random()*(500-200+1)+200);
+        return Math.floor(Math.random()*(500 - 200 + 1) + 200);
     }
 }
 
 function randomX(){
-        return Math.floor((Math.random() * 404 )+ 1);
+        return Math.floor((Math.random() * 404 ) + 1);
 }
 
 
